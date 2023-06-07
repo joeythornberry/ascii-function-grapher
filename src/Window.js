edcb6fa6b0a45f3cb9cd7a46aec28fe283b9298a
Luke Rozmajzl <lukerozmajzl@gmail.com>
2023-06-06 20:50:18 -0400

    create empty graph, turn it into a string, and display it

--- src/Window.js
+++ src/Window.js
@@ -9,7 +9,25 @@ class Window {
             this.Ymax = Ymax;
             this.scale = scale;
         }
-    } 
-}
+    }
+     emptyGrid() {
+        let grid = [] 
+            for(let y = this.Ymin; y < this.Ymax; y++) {
+                grid.push([])
+                for(let x = this.Xmin; x < this.Xmax; x++) {
+                    if(y === 0) {
+                        grid[y - this.Ymin].push("-")
+                        } else if(x === 0) {
+                            grid[y - this.Ymin].push("|")
+                        }
+                         else { 
+                            grid[y - this.Ymin].push("0")
+                        }
+                    
+                     }
+                };
+                return grid;
+            }
+    }
 
 export default Window;
--- src/asciiGraph.js
+++ src/asciiGraph.js
@@ -1,8 +1,15 @@
 import Window from "./Window";
 
 function asciiGraph(f,window) {
-    var graph = "NOT\nYET\nIMPLEMENTED";
-    return <div class = "asciiGraph">{graph}</div>;
+    var graph = window.emptyGrid();
+    let graph_string = "";
+    for(let y = 0; y < graph.length; y++) {
+        for(let x = 0; x < graph[y].length; x++) {
+            graph_string += graph[y][x]
+        }
+        graph_string += "\n";
+    }
+    return <div class = "asciiGraph">{graph_string}</div>;
 } 
 
 export default asciiGraph;
