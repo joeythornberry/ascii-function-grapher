4b285edb87952ccf2c3594e33c9b00cddc912526
Luke Rozmajzl <lukerozmajzl@gmail.com>
2023-06-30 19:43:35 -0400

    add color and fix spacing

--- src/asciiGraph.js
+++ src/asciiGraph.js
@@ -1,6 +1,6 @@
 import Window from "./Window";
 
-function asciiGraph(f,window) {
+function asciiGraph(f,window,function_color) {
     var graph = window.emptyGrid();
     for(let x = window.Xmin; x < window.Xmax; x++) {
         if(f((window.Xscale * x) - window.Xscale) / window.Yscale - (f(window.Xscale * x) / window.Yscale + 
@@ -25,19 +25,22 @@ function asciiGraph(f,window) {
         } else if(!left_low && right_low) {
             new_character = "\\";
         }
+            var new_character_html = <b style = {{color: function_color}}>{new_character}</b>
         if(f(window.Xscale * x) / window.Yscale < window.Ymax && f(window.Xscale * x) / window.Yscale > 
         window.Ymin) {
-            graph[Math.round((-1 * window.Ymin) - (f(x * window.Xscale) / window.Yscale))][x - window.Xmin] = new_character;
+            graph[Math.round((-1 * window.Ymin) - (f(x * window.Xscale) / window.Yscale))][x - window.Xmin] = new_character_html;
         }
     } 
-    let graph_string = "";
+    /**let graph_string = "";
     for(let y = 0; y < graph.length; y++) {
         for(let x = 0; x < graph[y].length; x++) {
             graph_string += graph[y][x]
         }
         graph_string += "\n";
-    }
-    return <div class = "asciiGraph">{graph_string}</div>;
+    }**/
+    return <div class = "asciiGraph">{
+        graph.map((line) => {return <div>{line}</div>})
+    }</div>;
 } 
 
 export default asciiGraph;
